c23feeb6f5ea440cc415051975493ddda4b8b4e6
dgarcia360 <dgarcia360@users.noreply.github.com>
2024-11-19 17:42:22 +0000

    deploy: ab0697c8f9ad67320d720aaf713df0ecb001190f

---
+++ assets/js/5e95c892.5281daa9.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkbiel=self.webpackChunkbiel||[]).push([[647],{7121:(e,s,r)=>{r.r(s),r.d(s,{default:()=>i});r(6540);var u=r(8215),a=r(1003),c=r(7559),l=r(2831),n=r(3818),t=r(4848);function i(e){return(0,t.jsx)(a.e3,{className:(0,u.A)(c.G.wrapper.docsPages),children:(0,t.jsx)(n.A,{children:(0,l.v)(e.route.routes)})})}}}]);
