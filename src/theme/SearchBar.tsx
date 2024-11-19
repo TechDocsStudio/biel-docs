76dcbb884786cefdfac79a29e319872af67b6558
David Garcia <hi@davidgarcia.dev>
2024-11-19 22:21:36 +0000

    chore: up

--- src/theme/SearchBar.tsx
+++ src/theme/SearchBar.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 export default function SearchBar() {
     // Replace with your <PROJECT_ID>
-    const projectId = '<PROJECT_ID>';
-    const headerTitle = 'Documentation chatbot';
+    const projectId = 'dl1kps0old';
+    const headerTitle = 'Biel.ai chatbot';
     // @ts-ignore: Not a React component
     return <biel-search-button project={projectId} button-style="rounded" header-title={headerTitle}>Search</biel-search-button>;
 }
