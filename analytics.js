85bf1f088b78252ae3d0eddbaff9ac04498be7ce
dgarcia360 <dgarcia360@users.noreply.github.com>
2025-06-17 16:42:34 +0000

    deploy: fbfb5f664ca44c2158c02b97163d579b37d0bef5

--- analytics.js
+++ analytics.js
@@ -1,1 +1,4 @@
-function gtag(){dataLayer.push(arguments)}window.dataLayer=window.dataLayer||[],gtag("js",new Date),gtag("config","G-1TMTNMCLSX");
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'G-1TMTNMCLSX');
